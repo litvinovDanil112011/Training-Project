88a832510d3aa010aec28121e35526e4e767284e
litvinovDanil112011 <danil112011@mail.ru>
2024-06-19 21:36:40 +0300

    addClass.

---
+++ Training Project/Managers/ConfigureViews.swift
@@ -0,0 +1,12 @@
+//
+//  ConfigureViews.swift
+//  Training Project
+//
+//  Created by Daniil Litvinov on 19.06.2024.
+//
+
+import UIKit
+
+class ConfigureViews {
+    static let share = ConfigureViews()
+}
